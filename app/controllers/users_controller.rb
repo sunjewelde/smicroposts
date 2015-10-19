adca39bc730a252761fd389e9c492c568981416d
sunjewelde <shibamuran@gmail.com>
2015-10-19 21:34:35 +0000

    add posting microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,20 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to root_path , notice: 'ユーザー情報を編集しました。'
+    else
+      render 'edit'
+    end
   end
   
   private
